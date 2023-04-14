f4600d2288429819e2a79dd2c8414f1bdb1f2443
LucasIriarte <lucasiriarte67@gmail.com>
2023-04-14 16:45:25 -0300

    Structure proyect

---
+++ App.jsx
@@ -0,0 +1,13 @@
+import { useEffect, useState } from "react"
+
+const RANDOM_FACT_URL = "https://catfact.ninja/fact"
+
+const App = () => {
+    
+    return (
+        <>
+            <h1>Portfolio</h1>
+        </>
+    )
+}
+export default App
