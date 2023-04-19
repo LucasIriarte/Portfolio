b108c162482d8ca0c2b78d74f8cadb2bb8415326
LucasIriarte <lucasiriarte67@gmail.com>
2023-04-19 17:40:37 -0300

    AboutMe

---
+++ components/AboutMe/AboutMe.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+
+const AboutMe = () => {
+    return (
+        <div className=" max-w-screen-xl mx-auto">
+        <h3 className="text-3xl">Sobre mi:</h3>
+        <p>Un apasionado por la tecnología en constante aprendizaje, enfrentándose a nuevos retos para probar y superar sus propios límites.</p>
+        </div>
+    ) 
+}
+
+export default AboutMe
--- components/Home/Home.jsx
+++ components/Home/Home.jsx
@@ -2,17 +2,18 @@ import React from "react";
 import Styles from "./Home.module.css"
 import img from "../Images/Lucas.png"
 import Header from "../Header/Header";
+import AboutMe from "../AboutMe/AboutMe";
 
 const Home = () => {
     return (
 
-        <main className="bg-color1 font-bold h-screen">
+        <main className="bg-color1 font-bold h-full">
 
             <Header/>
 
-            <div className="flex items-center justify-around max-w-screen-xl mx-auto pt-16">
-                <div className="">
-                    <h1 className="font-bold text-3xl text-transparent bg-clip-text bg-gradient-to-r from-color-main1 to-color-main2">Soy Lucas Ezequiel Iriarte</h1>
+            <div className="flex items-center justify-around max-w-screen-xl mx-auto pt-16 h-screen">
+                <div className="text-center">
+                    <h1 className="font-bold text-4xl text-transparent bg-clip-text bg-gradient-to-r from-color-main1 to-color-main2">Hola, soy Lucas Ezequiel Iriarte</h1>
                     <h2 className="font-medium text-transparent bg-clip-text bg-gradient-to-r from-color-main1 to-color-main2">FullStack Developer y apasionado por el Frontend</h2>
                 </div>
 
@@ -20,6 +21,8 @@ const Home = () => {
                     <img src={img} alt="Imagen Lucas Ezequiel Iriarte" className="w-full"/>
                 </div>
             </div>
+
+            <AboutMe/>
             
         </main>
     )
