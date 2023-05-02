99e141ca1e92ea8cfdfa9437bad9ecaa7ed20993
LucasIriarte <lucasiriarte67@gmail.com>
2023-05-02 17:30:20 -0300

    card styles made

--- components/Cards/Cards.jsx
+++ components/Cards/Cards.jsx
@@ -4,16 +4,12 @@ import Styles from './Cards.module.css'
 
 const Cards = (props) => {
     return (
-        <div className={`${Styles.containerCard} max-w-250px rounded-2xl overflow-hidden backdrop-blur-sm relative h-64 flex flex-col`}>
-            {/* <div className="h-full rounded-tl-1/2 rounded-bl-1/2 blur-xl w-1/3 bg-color-main2/[.3] absolute right-0 z-0">
-            </div>
-            <div className="h-full rounded-tr-1/2 rounded-br-1/2 blur-xl w-1/3 bg-color-main1/[.3] absolute left-0 z-0">
-            </div> */}
+        <div className={`${Styles.containerCard} max-w-250px rounded-2xl overflow-hidden h-64 flex flex-col`}>
             <h3 className="text-center">{props.title}</h3>
-            <div className="w-10/12 mx-auto z-10 rounded-md overflow-hidden relative">
-                <img src={props.img} alt="image-proyect" />
+            <div className={`w-10/12 mx-auto rounded-md overflow-hidden`}>
+                <img src={props.img} alt="image-proyect" className="bg-black"/>
             </div>
-            <button class="bg-gradient-to-r from-button-default via-button-default-clare to-button-default text-white font-bold py-2 px-4 rounded relative mt-8 w-10/12 mx-auto">
+            <button class="bg-gradient-to-r from-button-default via-button-default-clare to-button-default text-white font-bold py-2 px-4 rounded mt-8 w-10/12 mx-auto">
                 view
             </button>
         </div>
