5fcac65f8dff9137b7ff2007e662e91e382e8d7c
LucasIriarte <lucasiriarte67@gmail.com>
2023-05-04 14:20:22 -0300

    Technologies structure

---
+++ components/ContactMe/Contact.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+const ContactMe = () => {
+    return (
+        <>
+        <h3>Contacto</h3>
+        </>
+    )
+}
+
+export default ContactMe
--- components/Tecnologies/Tecnologies.jsx
+++ components/Tecnologies/Tecnologies.jsx
@@ -1,20 +1,46 @@
 import React from "react";
-import Styles from "./Tecnologies.module.css"
 
 const Tecnologies = () => {
     return (
         <div className="max-w-screen-xl mx-auto">
             <h3 className="text-3xl font-medium text-transparent bg-clip-text bg-gradient-to-r from-color-main1 to-color-main2 inline">Tecnologías:</h3>
-            <div className="flex">
-                <img src="../../public/reactIcon.svg" alt="Icon tecnologie" className={`${Styles.reactSvg} h-28 w-28`} />
-                <img src="../../public/css3Icon.svg" alt="" className={`${Styles.reactSvg} h-28 w-28`} />
-                <img src="../../public/html5Icon.svg" alt="" className={`${Styles.reactSvg} h-28 w-28`} />
-                <img src="../../public/expressJsIcon.svg" alt="" className={`${Styles.reactSvg} h-28 w-28`} />
-                <img src="../../public/gitIcon.svg" alt="" className={`${Styles.reactSvg} h-28 w-28`} />
-                <img src="../../public/nodeJsIcon.svg" alt="" className={`${Styles.reactSvg} h-28 w-28`} />
-                <img src="../../public/postgresqlIcon.svg" alt="" className={`${Styles.reactSvg} h-28 w-28`} />
-                <img src="../../public/redux-icon.png" alt="" className={`${Styles.reactSvg} h-28 w-28`} />
-                <img src="../../public/tailwindIcon.svg" alt="" className={`${Styles.reactSvg} h-28 w-28`} />
+            <div className="flex flex-wrap justify-center">
+                <div className="h-42 w-36 bg-white/[.02] flex justify-center align-center flex-col mx-12 my-3 rounded-xl border border-white/[.05]">
+                    <img src="../../public/reactIcon.svg" alt="Icon technology react"/>
+                    <h3 className="text-center">React.js</h3>
+                </div>
+                <div className="h-42 w-36 bg-white/[.02] flex justify-center align-center flex-col mx-12 my-3 rounded-xl border border-white/[.05]">
+                    <img src="../../public/css3Icon.svg" alt="Icon technoloy css"/>
+                    <h3 className="text-center">CSS3</h3>
+                </div>
+                <div className="h-42 w-36 bg-white/[.02] flex justify-center align-center flex-col mx-12 my-3 rounded-xl border border-white/[.05]">
+                    <img src="../../public/html5Icon.svg" alt="Icon technology html"/>
+                    <h3 className="text-center">HTML3</h3>
+                </div>
+                <div className="h-42 w-36 bg-white/[.02] flex justify-center align-center flex-col mx-12 my-3 rounded-xl border border-white/[.05]">
+                    <img src="../../public/expressJsIcon.svg" alt="Icon technology expresJs"/>
+                    <h3 className="text-center">Express.js</h3>
+                </div>
+                <div className="h-42 w-36 bg-white/[.02] flex justify-center align-center flex-col mx-12 my-3 rounded-xl border border-white/[.05]">
+                    <img src="../../public/gitIcon.svg" alt="Icon technology GIT"/>
+                    <h3 className="text-center">GIT</h3>
+                </div>
+                <div className="h-42 w-36 bg-white/[.02] flex justify-center align-center flex-col mx-12 my-3 rounded-xl border border-white/[.05]">
+                    <img src="../../public/nodeJsIcon.svg" alt="Icon technology nodeJs"/>
+                    <h3 className="text-center">Node.js</h3>
+                </div>
+                <div className="h-42 w-36 bg-white/[.02] flex justify-center align-center flex-col mx-12 my-3 rounded-xl border border-white/[.05]">
+                    <img src="../../public/postgresqlIcon.svg" alt="Icon technology postgresql"/>
+                    <h3 className="text-center">PostgresSQL</h3>
+                </div>
+                <div className="h-42 w-36 bg-white/[.02] flex justify-center align-center flex-col mx-12 my-3 rounded-xl border border-white/[.05]">
+                    <img src="../../public/redux-icon.png" alt="Icon technology redux"/>
+                    <h3 className="text-center">Redux.js</h3>
+                </div>
+                <div className="h-42 w-36 bg-white/[.02] flex justify-center align-center flex-col mx-12 my-3 rounded-xl border border-white/[.05]">
+                    <img src="../../public/tailwindIcon.svg" alt="Icon technology tailwind"/>
+                    <h3 className="text-center">Tailwindcss</h3>
+                </div>
             </div>
         </div>
     )
