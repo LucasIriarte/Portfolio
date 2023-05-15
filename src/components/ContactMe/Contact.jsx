3b834e5d3189e6a85552e5155f18860ced3567d7
LucasIriarte <lucasiriarte67@gmail.com>
2023-05-15 16:39:01 -0300

    Fix images ContactMe

--- src/components/ContactMe/Contact.jsx
+++ src/components/ContactMe/Contact.jsx
@@ -1,4 +1,7 @@
 import React from "react";
+import GitHubIcon from "../../assets/githubIcon.svg"
+import EmailIcon from "../../assets/correoIcon.png"
+import LinkedinIcon from "../../assets/linkedinIcon.svg"
 
 
 const ContactMe = () => {
@@ -8,19 +11,19 @@ const ContactMe = () => {
             <div className="flex mt-4">
                 <div className="hover:scale-105 transition-all duration-300 mx-8 ml-0">
                     <a href="mailto:lucasiriarte67@gmail.com" target="_blank" className="hover:scale-105 transition-all duration-300">
-                        <img src="../../public/correoIcon.png" alt="Correo electronico icono" className="w-24 ml-0 mx-2" />
+                        <img src={ EmailIcon } alt="Correo electronico icono" className="w-24 ml-0 mx-2" />
                         <h4 className="text-2xl font-medium text-transparent bg-clip-text bg-gradient-to-r from-color-main1 to-color-main2 text-center">Email</h4>
                     </a>
                 </div>
                 <div className="hover:scale-105 transition-all duration-300 mx-8">
                     <a href="https://github.com/LucasIriarte" target="_blank" className="hover:scale-105 transition-all duration-300">
-                        <img src="../../public/githubIcon.svg" alt="" className="w-24 mx-2" />
+                        <img src={ GitHubIcon } alt="" className="w-24 mx-2" />
                         <h4 className="text-2xl font-medium text-transparent bg-clip-text bg-gradient-to-r from-color-main1 to-color-main2 text-center">GitHub</h4>
                     </a>
                 </div>
                 <div className="hover:scale-105 transition-all duration-300 mx-8">
                     <a href="https://www.linkedin.com/in/lucas-iriarte/" target="_blank" className="hover:scale-105 transition-all duration-300">
-                        <img src="../../public/linkedinIcon.svg" alt="" className="w-24 mx-2" />
+                        <img src={ LinkedinIcon } alt="" className="w-24 mx-2" />
                         <h4 className="text-2xl font-medium text-transparent bg-clip-text bg-gradient-to-r from-color-main1 to-color-main2 text-center">Linkedin</h4>
                     </a>
                 </div>
