5988626f76630b83a8687d08993c44149b610ede
LucasIriarte <lucasiriarte67@gmail.com>
2023-05-17 16:40:11 -0300

    Styles img

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -6,6 +6,8 @@ import Tecnologies from "../Tecnologies/Tecnologies";
 import ContactMe from "../ContactMe/Contact";
 import Lucas from "../../assets/Lucas.png"
 import Footer from "../Footer/Footer";
+import Styles from "./Home.module.css"
+import Variables from "../../Variables.css"
 
 const Home = () => {
     return (
@@ -14,13 +16,13 @@ const Home = () => {
 
             <Header/>
 
-            <div className="flex items-center justify-around max-w-screen-xl mx-auto pt-16 h-screen">
+            <div className="flex flex-col items-center justify-center max-w-screen-xl mx-auto h-screen">
                 <div className="text-center">
                     <h1 className="font-bold text-4xl text-transparent bg-clip-text bg-gradient-to-r from-color-main1 to-color-main2">Hola, soy Lucas Ezequiel Iriarte</h1>
                     <h2 className="font-medium text-transparent bg-clip-text bg-gradient-to-r from-color-main1 to-color-main2">FullStack Developer y apasionado por el Frontend</h2>
                 </div>
 
-                <div className="w-1/2 max-w-xs">
+                <div className={`mt-16 w-1/2 max-w-xs rounded-full bg-gradient-to-r from-color-main1 to-color-main2 p-1 ${Styles.boxShadowPhoto}`}>
                     <img src={Lucas} alt="Imagen Lucas Ezequiel Iriarte" className="w-full"/>
                 </div>
             </div>
