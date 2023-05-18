a8edede37b3dd93f82be0d7af3afcbbe63dbb920
LucasIriarte <lucasiriarte67@gmail.com>
2023-05-18 13:23:51 -0300

    Footer styles

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 import Styles from "./Footer.module.css"
 import Variables from "../../Variables.css?inline"
+import GitHubIcon from "../../assets/githubIcon.svg"
+import EmailIcon from "../../assets/correoIcon.png"
+import LinkedinIcon from "../../assets/linkedinIcon.svg"
 
 
 const Footer = () => {
@@ -8,24 +11,27 @@ const Footer = () => {
 
         <footer className={`shadow bg-color5/[.3]`}>
             <div className={`my-6 sm:mx-auto lg:my-8 ${Styles.AnimFooter}`}></div>
-            <div className="w-full mx-auto p-4 md:py-8">
-                <div className="sm:flex sm:items-center sm:justify-between">
-                    <ul className="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0 dark:text-gray-400">
-                        <li>
-                            <a href="#" className="mr-4 hover:underline md:mr-6 ">About</a>
-                        </li>
-                        <li>
-                            <a href="#" className="mr-4 hover:underline md:mr-6">Privacy Policy</a>
-                        </li>
-                        <li>
-                            <a href="#" className="mr-4 hover:underline md:mr-6 ">Licensing</a>
-                        </li>
-                        <li>
-                            <a href="#" className="hover:underline">Contact</a>
-                        </li>
-                    </ul>
+            <div className="flex items-center flex-col">
+                <h3 className="text-3xl font-medium text-transparent bg-clip-text bg-gradient-to-r from-color-main1 to-color-main2 inline">Contactame</h3>
+                <div className="flex">
+                    <div className="hover:scale-105 transition-all duration-300 sm:mx-8">
+                        <a href="mailto:lucasiriarte67@gmail.com" target="_blank" className="hover:scale-105 transition-all duration-300">
+                            <img src={EmailIcon} alt="Correo electronico icono" className="w-24 mx-2" />
+                        </a>
+                    </div>
+                    <div className="hover:scale-105 transition-all duration-300 sm:mx-8">
+                        <a href="https://github.com/LucasIriarte" target="_blank" className="hover:scale-105 transition-all duration-300">
+                            <img src={GitHubIcon} alt="" className="w-24 mx-2" />
+                        </a>
+                    </div>
+                    <div className="hover:scale-105 transition-all duration-300 sm:mx-8">
+                        <a href="https://www.linkedin.com/in/lucas-iriarte/" target="_blank" className="hover:scale-105 transition-all duration-300">
+                            <img src={LinkedinIcon} alt="" className="w-24 mx-2" />
+                        </a>
+                    </div>
                 </div>
             </div>
+
         </footer>
 
 
