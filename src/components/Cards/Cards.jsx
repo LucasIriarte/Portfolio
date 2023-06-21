11b0c07be6d3fb3fc47a5c5fcd6fd210c1af8cee
LucasIriarte <lucasiriarte67@gmail.com>
2023-06-21 20:55:55 -0300

    proyects and cards modify

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -5,28 +5,29 @@ import Styles from './Cards.module.css'
 const Cards = (props) => {
     const [moreInfo, setMoreInfo] = useState(false)
     const handleMoreInfo = () => {
-        moreInfo?setMoreInfo(false):setMoreInfo(true)
+        moreInfo ? setMoreInfo(false) : setMoreInfo(true)
     }
-    useEffect(()=>{
+    useEffect(() => {
         console.log(moreInfo)
     }, [moreInfo])
     return (
-            <div className={`${Styles.containerCard} min-w-250px max-w-250px rounded-2xl overflow-hidden h-64 flex flex-col min-h-64`}>
-                <h3 className="text-center">{props.title}</h3>
-                <div className={moreInfo?`hidden`:`w-10/12 mx-auto rounded-md overflow-hidden`}>
-                    <img src={props.img} alt="image-proyect" className="bg-black" />
-                </div>
-                <p className={moreInfo?"px-4 overflow-auto":"hidden"}>
-                    Proyecto en el que se muestran perros traidos desde una api externa y permite crear propios perros, se utilizó: JavaScript, React, Redux, Sequalize, Express y PostgreSQL.
-                    Podes verlo <a href="#" className="decoration-solid text-blue-900" >acá</a>.
-                </p>
-                {moreInfo?<button className="bg-gradient-to-r from-button-default via-button-default-clare to-button-default text-white font-bold py-2 px-4 rounded mt-8 w-10/12 mx-auto" onClick={handleMoreInfo}>
-                    menos info
-                </button>:<button className="bg-gradient-to-r from-button-default via-button-default-clare to-button-default text-white font-bold py-2 px-4 rounded mt-8 w-10/12 mx-auto" onClick={handleMoreInfo}>
-                    más info
-                </button>}
-                
+        <div className={`${Styles.containerCard} min-w-250px max-w-250px rounded-2xl overflow-hidden h-64 flex flex-col min-h-64`}>
+            <h3 className="text-center">{props.title}</h3>
+            <div className={moreInfo ? `hidden` : `w-10/12 mx-auto rounded-md overflow-hidden`}>
+                <img src={props.img} alt="image-proyect" className="bg-black" />
             </div>
+            <p className={moreInfo ? "px-4 overflow-auto" : "hidden"}>
+                {props.moreInfo} 
+                <a href={props.deploy} target="blank" className="decoration-solid text-blue-900 block text-white text-center bg-gradient-to-r from-button-default via-button-default-clare to-button-default text-white rounded" >Deploy</a>
+                <a href={props.gitHub} target="blank" className="decoration-solid text-blue-900 cursor-pointer block text-white text-center bg-gradient-to-r from-button-default via-button-default-clare to-button-default text-white rounded mt-3" >GitHub</a>
+            </p>
+            {moreInfo ? <button className="bg-gradient-to-r from-button-default via-button-default-clare to-button-default text-white font-bold py-2 px-4 rounded mt-8 w-10/12 mx-auto" onClick={handleMoreInfo}>
+                menos info
+            </button> : <button className="bg-gradient-to-r from-button-default via-button-default-clare to-button-default text-white font-bold py-2 px-4 rounded mt-8 w-10/12 mx-auto" onClick={handleMoreInfo}>
+                más info
+            </button>}
+
+        </div>
     )
 }
 
--- src/components/Proyects/Proyects.jsx
+++ src/components/Proyects/Proyects.jsx
@@ -1,15 +1,19 @@
 import React from "react";
 import Cards from "../Cards/Cards";
 import PiDogs from "../../assets/piDogs.jpg"
+import pfRadiatorSprings from "../../assets/pfRadiatorSprings.jpg"
 
 
 const Proyects = () => {
     return (
-        <div id="Proyects" className="max-w-screen-xl mx-auto flex flex-col items-center md:items-start md:pl-12 scroll-mt-20">
-           <h3 className="text-3xl font-medium text-transparent bg-clip-text bg-gradient-to-r from-color-main1 to-color-main2 inline">Proyectos</h3> 
-           <div className="mt-8">
-                <Cards title="PI dogs" img={ PiDogs }/>
-           </div>
+        <div id="Proyects" className="max-w-screen-xl mx-auto mt-24 flex flex-col scroll-mt-20">
+            <h3 className="text-3xl mx-auto font-medium text-transparent bg-clip-text bg-gradient-to-r from-color-main1 to-color-main2 inline md:ml-12">Proyectos</h3>
+            <div className="flex flex-wrap justify-center mt-8 w-full sm:justify-around md:justify-start md:ml-12">
+                <Cards title="PI dogs" img={PiDogs} moreInfo="Proyecto en el que se muestran perros traidos desde una api externa y permite crear propios perros, se utilizó: JavaScript, React, Redux, Sequalize, Express y PostgreSQL." deploy="https://pi-dogs-rouge.vercel.app" gitHub="https://github.com/LucasIriarte/PI-Dogs" />
+                <div className="ml-12">
+                    <Cards title="PF Radiator Springs" img={pfRadiatorSprings} moreInfo="Proyecto grupal en donde la idea se basa en una página para la venta y compra de automoviles, se creó una api en la que se pueden guardar nuevos autos a la venta y son mostrados de manera automatica tras un pago por anunciarlo." deploy="https://pf-henryss.vercel.app/" gitHub="https://github.com/Knd0/PF_Henryss" />
+                </div>
+            </div>
         </div>
     )
 }
