8800ea5d2c3026c01ce62219e9585208d1587d74
LucasIriarte <lucasiriarte67@gmail.com>
2024-08-06 21:08:00 -0300

    fix: vite config problem with deploy

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,8 @@ export default defineConfig({
   plugins: [react()],
   server: {
     mimeTypes: {
-      'js': 'application/javascript'
+      'js': 'application/javascript',
+      'jsx': 'application/javascript'
     }
   }
 })
